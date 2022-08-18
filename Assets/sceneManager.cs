2880b5d4bb131a22a4d6e916ea87871dcae2557c
Ackrawin B <63220854+BXSS101@users.noreply.github.com>
2022-08-18 17:28:21 +0700

    add change scene script

---
+++ Assets/sceneManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneManager : MonoBehaviour {
+    public void Scene1() {
+        SceneManager.LoadScene("LoginSuccess");
+    }
+}
