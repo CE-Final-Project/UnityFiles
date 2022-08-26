ebf78618ab7af8e7158e2c528ecd9383f799ff2d
Ackrawin B <63220854+BXSS101@users.noreply.github.com>
2022-08-26 08:55:19 +0700

    add full screen toggle button

---
+++ Assets/Script/fullScreenToggle.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fullScreenToggle : MonoBehaviour{
+    public void toggle(bool is_fullScreen) {
+        Screen.fullScreen = is_fullScreen;
+        if(is_fullScreen) {
+            print("full screen");
+            Screen.SetResolution(1920, 1080, true);
+        }
+        else {
+            print("not full screen");
+            Screen.SetResolution(1280, 720, false);
+        }
+    }
+}
