d0645647999c817f000f82ffaebbd4e6ecaa7efb
Ackrawin B <63220854+BXSS101@users.noreply.github.com>
2022-09-19 18:07:20 +0700

    add noise generator code

---
+++ Assets/Script/biomePreset.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CreateAssetMenu(fileName = "Biome Preset", menuName = "New Biome Preset")]
+public class biomePreset : MonoBehaviour
+{
+    
+}
