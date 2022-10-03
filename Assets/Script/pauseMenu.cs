a4c9461c997d647c6191e114f3a35cb20457a153
Ackrawin B <63220854+BXSS101@users.noreply.github.com>
2022-10-03 12:05:32 +0700

    [Update] pause button varies on device type

--- Assets/Script/pauseMenu.cs
+++ Assets/Script/pauseMenu.cs
@@ -17,18 +17,37 @@ public class pauseMenu : MonoBehaviour
                 Pause();
             }
         }
+        if (SystemInfo.deviceType == DeviceType.Handheld && gameIsPaused == false)
+        {
+            print("on mobile and not paused");
+            pauseButtonUI.SetActive(true);
+        }
     }
 
     public void Resume() {
         pauseMenuUI.SetActive(false);
-        //pauseButtonUI.SetActive(true);
+        if (SystemInfo.deviceType == DeviceType.Handheld)
+        {
+            pauseButtonUI.SetActive(true);
+        }
+        else if (SystemInfo.deviceType == DeviceType.Desktop)
+        {
+            pauseButtonUI.SetActive(false);
+        }
         Time.timeScale = 1f;
         gameIsPaused = false;
     }
     
     public void Pause() {
         pauseMenuUI.SetActive(true);
-        //pauseButtonUI.SetActive(false);
+        if (SystemInfo.deviceType == DeviceType.Handheld)
+        {
+            pauseButtonUI.SetActive(false);
+        }
+        else if (SystemInfo.deviceType == DeviceType.Desktop)
+        {
+            pauseButtonUI.SetActive(false);
+        }
         Time.timeScale = 0f;
         gameIsPaused = true;
     }
