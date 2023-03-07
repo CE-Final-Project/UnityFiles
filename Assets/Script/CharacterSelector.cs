c61436b4ed76449db910a79bef2b8c1d7be18375
Ackrawin B <63220854+BXSS101@users.noreply.github.com>
2023-03-07 12:54:26 +0700

    feat(DDA_Debug_overlay):added Debug overlay toggle by press F

---
+++ Assets/Script/CharacterSelector.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterSelector : MonoBehaviour
+{
+    public CharacterBox[] characterBoxes;
+
+    private void Start()
+    {
+        // Set all character boxes to unselected at start
+        foreach (CharacterBox characterBox in characterBoxes)
+        {
+            characterBox.SetSelected(false);
+        }
+    }
+
+    public void SelectCharacter(CharacterBox selectedCharacterBox)
+    {
+        // Set all character boxes to unselected except for the selected one
+        foreach (CharacterBox characterBox in characterBoxes)
+        {
+            if (characterBox == selectedCharacterBox)
+            {
+                characterBox.SetSelected(true);
+            }
+            else
+            {
+                characterBox.SetSelected(false);
+            }
+        }
+    }
+}
