16f8a0bba1b0eb59ac9a2611d84c56be98d2f209
Kenta420 <poomipat002@gmail.com>
2023-03-12 17:31:39 +0700

    save

--- Assets/Script/NGO/InGameRunner.cs
+++ Assets/Script/NGO/InGameRunner.cs
@@ -1,13 +1,81 @@
 ﻿using System;
+using System.Collections.Generic;
 using Unity.Netcode;
+using UnityEngine;
 
 namespace Script.NGO
 {
     public class InGameRunner : NetworkBehaviour
     {
+        [SerializeField] private GameObject playerPrefab;
+        
+        [SerializeField] private NetworkDataStore networkDataStore;
+        
+        public Action OnGameBeginning;
+        private Action _onConnectionVerified;
+        private Action _onGameEnd;
+        
+        private int _expectedPlayerCount;
+        private bool? _canSpawnInGameObjects;
+        private bool _hasConnected;
+        
+        private PlayerData _localPlayerData;
+        
+        private static InGameRunner _instance;
+
+        public static InGameRunner Instance
+        {
+            get
+            {
+                if (_instance is null) return _instance = FindObjectOfType<InGameRunner>();
+                return _instance;
+            }
+        }
+
         public void Initialize(Action onConnectionVerified, int expectedPlayerCount, Action onGameBegin, Action onGameEnd, LocalPlayer localUser)
         {
-            
+            _onConnectionVerified = onConnectionVerified;
+            _expectedPlayerCount = expectedPlayerCount;
+            OnGameBeginning = onGameBegin;
+            _onGameEnd = onGameEnd;
+            _localPlayerData = new PlayerData(localUser.DisplayName.Value, 0, 10, localUser.Character.Value);
+        }
+        
+        public override void OnNetworkSpawn()
+        {
+            if (IsHost)
+                FinishInitialize();
+            _localPlayerData = new PlayerData(_localPlayerData.Name, NetworkManager.Singleton.LocalClientId, _localPlayerData.Health, _localPlayerData.CharacterType);
+            VerifyConnection_ServerRpc(_localPlayerData.Id);
+        }
+
+        public override void OnNetworkDespawn()
+        {
+            _onGameEnd();
+        }
+        
+        private void FinishInitialize()
+        {
+           
+        }
+        
+        [ServerRpc(RequireOwnership = false)]
+        private void VerifyConnection_ServerRpc(ulong clientId)
+        {
+            VerifyConnection_ClientRpc(clientId);
+        }
+        
+        [ClientRpc]
+        private void VerifyConnection_ClientRpc(ulong clientId)
+        {
+            if (clientId == _localPlayerData.Id)
+                VerifyConnectionConfirm_ServerRpc(_localPlayerData);
+        }
+        
+        [ServerRpc(RequireOwnership = false)]
+        private void VerifyConnectionConfirm_ServerRpc(PlayerData clientData)
+        {
+            // TODO: Initialize player character and spawn it in the game
         }
     }
 }
---
+++ Assets/Script/NGO/PlayerData.cs
@@ -0,0 +1,36 @@
+﻿using Script.Game;
+using Unity.Netcode;
+
+namespace Script.NGO
+{
+    public class PlayerData : INetworkSerializable
+    {
+        public ulong Id;
+        public string Name;
+        public float Health;
+        public CharacterType CharacterType;
+        public int Score;
+        
+        public PlayerData()
+        {
+        }
+        
+        public PlayerData(string name, ulong id, float health, CharacterType characterType, int score = 0)
+        {
+            Id = id;
+            Name = name;
+            Health = health;
+            CharacterType = characterType;
+            Score = score;
+        }
+        
+        public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
+        {
+            serializer.SerializeValue(ref Id);
+            serializer.SerializeValue(ref Name);
+            serializer.SerializeValue(ref Health);
+            serializer.SerializeValue(ref CharacterType);
+            serializer.SerializeValue(ref Score);   
+        }
+    }
+}
