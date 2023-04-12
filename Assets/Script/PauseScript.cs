96a1389ebf70509bebfe17c737b0df4c1de9df1f
Ackrawin B <63220854+BXSS101@users.noreply.github.com>
2023-04-12 12:58:52 +0700

    edit AC_Effect states and HUD Data

--- Assets/Script/PauseScript.cs
+++ Assets/Script/PauseScript.cs
@@ -1,10 +1,21 @@
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class PauseScript : MonoBehaviour
 {
     private bool overlayCheck = false;
     public GameObject overlayObj;
+    public TextMeshProUGUI playerPositionText;
+    public TextMeshProUGUI cameraPositionText;
+    private GameObject playerAvatar;
+    private GameObject playerCamera;
 
+    void Start()
+    {
+        playerAvatar = GameObject.FindGameObjectWithTag("Player");
+        playerCamera = GameObject.FindGameObjectWithTag("VirtualCamera");
+    }
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.F))
@@ -28,6 +39,26 @@ public class PauseScript : MonoBehaviour
                 Debug.Log("Overlay OFF");
                 overlayObj.SetActive(false);
             }
+        }
+
+        //Update Player Position on HUD
+        if (playerAvatar != null)
+        {
+            // Get the transform position of the player avatar
+            Vector2 playerPosition = playerAvatar.transform.position;
+
+            // Update the text UI component with the player position
+            playerPositionText.text = "Player position\n " + playerPosition.ToString();
+        }
+
+        //Update Camera Position on HUD
+        if (playerCamera != null)
+        {
+            // Get the transform position of the player avatar
+            Vector2 cameraPosition = playerCamera.transform.position;
+
+            // Update the text UI component with the player position
+            cameraPositionText.text = "Camera position\n " + cameraPosition.ToString();
         }
     }
 }
