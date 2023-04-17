230004868d2e548915e5c102fb164516b6337afb
Kenta420 <poomipat002@gmail.com>
2023-04-17 18:26:06 +0700

    feat: added PlayersStats writer

--- Assets/Script/Game/GameplayObject/RuntimeDataContainers/PlayersStats.cs
+++ Assets/Script/Game/GameplayObject/RuntimeDataContainers/PlayersStats.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using TMPro;
 using UnityEngine;
 
@@ -30,65 +32,67 @@ namespace Script.Game.GameplayObject.RuntimeDataContainers
             return ClientId.GetHashCode();
         }
     }
+    
+    public class PlayerStats
+    {
+        public string CharacterType { get; private set; }
+        public int KillCount { get; private set; }
+        public int DeathCount { get; private set; }
+        public int DamageDealt { get; private set; }
+        public int DamageTaken { get; private set; }
+        public int HealingDone { get; private set; }
+        public int HealingTaken { get; private set; }
+
+        public PlayerStats(string characterType, int killCount, int deathCount, int damageDealt, int damageTaken, int healingDone, int healingTaken)
+        {
+            CharacterType = characterType;
+            KillCount = killCount;
+            DeathCount = deathCount;
+            DamageDealt = damageDealt;
+            DamageTaken = damageTaken;
+            HealingDone = healingDone;
+            HealingTaken = healingTaken;
+        }
+            
+        public void AddKill()
+        {
+            KillCount++;
+        }
+            
+        public void AddDeath()
+        {
+            DeathCount++;
+        }
+            
+        public void AddDamageDealt(int damage)
+        {
+            DamageDealt += damage;
+        }
+            
+        public void AddDamageTaken(int damage)
+        {
+            DamageTaken += damage;
+        }
+            
+        public void AddHealingDone(int healing)
+        {
+            HealingDone += healing;
+        }
+            
+        public void AddHealingTaken(int healing)
+        {
+            HealingTaken += healing;
+        }
+    }
 
 
     public class PlayersStats : MonoBehaviour
     {
-        public struct PlayerStats
-        {
-            public string CharacterType { get; private set; }
-            public int KillCount { get; private set; }
-            public int DeathCount { get; private set; }
-            public int DamageDealt { get; private set; }
-            public int DamageTaken { get; private set; }
-            public int HealingDone { get; private set; }
-            public int HealingTaken { get; private set; }
-
-            public PlayerStats(string characterType, int killCount, int deathCount, int damageDealt, int damageTaken, int healingDone, int healingTaken)
-            {
-                CharacterType = characterType;
-                KillCount = killCount;
-                DeathCount = deathCount;
-                DamageDealt = damageDealt;
-                DamageTaken = damageTaken;
-                HealingDone = healingDone;
-                HealingTaken = healingTaken;
-            }
-            
-            public void AddKill()
-            {
-                KillCount++;
-            }
-            
-            public void AddDeath()
-            {
-                DeathCount++;
-            }
-            
-            public void AddDamageDealt(int damage)
-            {
-                DamageDealt += damage;
-            }
-            
-            public void AddDamageTaken(int damage)
-            {
-                DamageTaken += damage;
-            }
-            
-            public void AddHealingDone(int healing)
-            {
-                HealingDone += healing;
-            }
-            
-            public void AddHealingTaken(int healing)
-            {
-                HealingTaken += healing;
-            }
-        }
-        
-         public static PlayersStats Instance { get; private set; }
-         
-         private Dictionary<PlayerId, PlayerStats> _playerStatsMap = new Dictionary<PlayerId, PlayerStats>();
+        public static PlayersStats Instance { get; private set; }
+        private Dictionary<PlayerId, PlayerStats> _playerStatsMap = new Dictionary<PlayerId, PlayerStats>();
+        private DateTime _startTime;
+        private DateTime _playTime;
+             
             
         private void Awake()
         {
@@ -103,6 +107,44 @@ namespace Script.Game.GameplayObject.RuntimeDataContainers
             Instance = this;
         }
         
+        private void Start()
+        {
+            _startTime = DateTime.Now;
+            StartCoroutine(WriteDataToCsvCoroutine());
+        }
+        
+        private IEnumerator WriteDataToCsvCoroutine()
+        {
+            // Write the data to a CSV file
+                string filePath = Application.persistentDataPath + $"/players_stats{_startTime:yyyy-M-d-HH-mm-ss}.csv";
+                bool fileExists = File.Exists(filePath);
+                
+                using (StreamWriter writer = new StreamWriter(filePath, true))
+                {
+                    if (!fileExists)
+                    {
+                      writer.WriteLine("Play Time (Second),PlayerId,CharacterType,KillCount,DeathCount,DamageDealt,DamageTaken,HealingDone,HealingTaken");   
+                    }
+
+                    while (true)
+                    {
+                        // Wait for 2 minutes before writing data again
+                        yield return new WaitForSecondsRealtime(120);
+                        
+                        string playTime = DateTime.Now.Subtract(_startTime).Seconds.ToString();
+
+                        // Write data for each player
+                        foreach (var playerStats in _playerStatsMap)
+                        {
+                            string row = $"{playTime}, {playerStats.Key.ClientId},{playerStats.Value.CharacterType},{playerStats.Value.KillCount},{playerStats.Value.DeathCount},{playerStats.Value.DamageDealt},{playerStats.Value.DamageTaken},{playerStats.Value.HealingDone},{playerStats.Value.HealingTaken}";
+                            writer.WriteLine(row);
+                        }
+                        writer.Flush();
+                        Debug.Log($"Wrote player stats to {filePath}");
+                    }
+                }
+        }
+
         public void ClearData()
         {
             _playerStatsMap.Clear();    
@@ -144,19 +186,21 @@ namespace Script.Game.GameplayObject.RuntimeDataContainers
         public void AddDamageDealt(ulong clientId, int damage)
         {
             PlayerId playerId = new(clientId);
-            if (_playerStatsMap.TryGetValue(playerId, out PlayerStats playerStats))
+            if (!_playerStatsMap.ContainsKey(playerId))
             {
-                playerStats.AddDamageDealt(damage);
+                Debug.LogError($"Player {clientId} does not exist");
             }
+            _playerStatsMap[playerId].AddDamageDealt(damage);
         }
         
         public void AddDamageTaken(ulong clientId, int damage)
         {
             PlayerId playerId = new(clientId);
-            if (_playerStatsMap.TryGetValue(playerId, out PlayerStats playerStats))
+            if (!_playerStatsMap.ContainsKey(playerId))
             {
-                playerStats.AddDamageTaken(damage);
+                Debug.LogError($"Player {clientId} does not exist");
             }
+            _playerStatsMap[playerId].AddDamageTaken(damage);
         }
         
         public void AddHealingDone(ulong clientId, int healing)
