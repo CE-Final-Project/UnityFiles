1cacfaaaf0073520222bde17d41ad51df8f27617
Kenta420 <poomipat002@gmail.com>
2023-04-18 14:54:34 +0700

    feat: new aoe action now can make a damage

--- Assets/Script/Game/Actions/Input/AoeActionInput.cs
+++ Assets/Script/Game/Actions/Input/AoeActionInput.cs
@@ -9,20 +9,89 @@ namespace Script.Game.Actions.Input
         [SerializeField] private GameObject inRangeVisualization;
         [SerializeField] private GameObject outOfRangeVisualization;
 
-        private Camera _camera;
-        
-        private bool _receivedMouseDownEvent;
+        Camera m_Camera;
 
-        private NavMeshHit _navMeshHit;
+        //The general action system works on MouseDown events (to support Charged Actions), but that means that if we only wait for
+        //a mouse up event internally, we will fire as part of the same UI click that started the action input (meaning the user would
+        //have to drag her mouse from the button to the firing location). Tracking a mouse-down mouse-up cycle means that a user can
+        //click on the NavMesh separately from the mouse-click that engaged the action (which also makes the UI flow equivalent to the
+        //flow from hitting a number key).
+        bool m_ReceivedMouseDownEvent;
+
+        NavMeshHit m_NavMeshHit;
+
+        // plane that has normal pointing up on y, that is 0 distance units displaced from origin
+        // this is also the same height as the NavMesh baked in-game
+        static readonly Plane k_Plane = new(Vector3.forward, 0f);
         
-        private static readonly Plane Plane = new Plane(Vector3.up, 0);
-        
-        private void Start()
+        void Start()
         {
-            float redius = GameDataSource.Instance.GetActionPrototypeByID(ActionPrototypeID).Config.Radius;
-            
-            transform.localScale = new Vector3(redius * 2, redius * 2, redius * 2);
-            _camera = Camera.main;
+            var radius = GameDataSource.Instance.GetActionPrototypeByID(ActionPrototypeID).Config.Radius;
+
+            transform.localScale = new Vector3(radius * 2, radius * 2, radius * 2);
+            m_Camera = Camera.main;
+        }
+
+        void Update()
+        {
+            if (PlaneRaycast(k_Plane, m_Camera.ScreenPointToRay(UnityEngine.Input.mousePosition), out Vector2 pointOnPlane) &&
+                NavMesh.SamplePosition(pointOnPlane, out m_NavMeshHit, 2f, NavMesh.AllAreas))
+            {
+                transform.position = m_NavMeshHit.position;
+            }
+
+            float range = GameDataSource.Instance.GetActionPrototypeByID(ActionPrototypeID).Config.Range;
+            bool isInRange = (Origin - transform.position).sqrMagnitude <= range * range;
+            inRangeVisualization.SetActive(isInRange);
+            outOfRangeVisualization.SetActive(!isInRange);
+
+            // wait for the player to click down and then release the mouse button before actually taking the input
+            if (UnityEngine.Input.GetMouseButtonDown(0))
+            {
+                m_ReceivedMouseDownEvent = true;
+            }
+
+            if (UnityEngine.Input.GetMouseButtonUp(0) && m_ReceivedMouseDownEvent)
+            {
+                if (isInRange)
+                {
+                    var data = new ActionRequestData
+                    {
+                        Position = transform.position,
+                        ActionID = ActionPrototypeID,
+                        ShouldQueue = false,
+                        TargetIDs = null
+                    };
+                    SendInput(data);
+                }
+                Destroy(gameObject);
+                return;
+            }
+        }
+
+        /// <summary>
+        /// Utility method to simulate a raycast to a given plane. Does not involve a Physics-based raycast.
+        /// </summary>
+        /// <remarks> Based on documented example here: https://docs.unity3d.com/ScriptReference/Plane.Raycast.html
+        /// </remarks>
+        /// <param name="plane"></param>
+        /// <param name="ray"></param>
+        /// <param name="pointOnPlane"></param>
+        /// <returns> true if intersection point lies inside NavMesh; false otherwise </returns>
+        static bool PlaneRaycast(Plane plane, Ray ray, out Vector2 pointOnPlane)
+        {
+            // validate that this ray intersects plane
+            if (plane.Raycast(ray, out var enter))
+            {
+                // get the point of intersection
+                pointOnPlane = ray.GetPoint(enter);
+                return true;
+            }
+            else
+            {
+                pointOnPlane = Vector2.zero;
+                return false;
+            }
         }
     }
 }
