f1b46f547c514be6ae6baa1b61feeec783170145
Kenta420 <poomipat002@gmail.com>
2023-04-19 06:51:15 +0700

    add new file sprite

---
+++ Assets/Script/Game/GameplayObject/AOEs/FXAoe.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+namespace Script.Game.GameplayObject.AOEs
+{
+    public class FXAoe : MonoBehaviour
+    {
+        
+        private float m_Duration = 1.1f; // default duration
+        
+        public void Initialize(float duration)
+        {
+            m_Duration = duration;
+        }
+
+
+        private void Update()
+        {
+            m_Duration -= Time.deltaTime;
+            if (m_Duration <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Script/GameState/PostGameState.cs
@@ -0,0 +1,42 @@
+﻿using Script.ConnectionManagement;
+using Script.Game.GameplayObject.RuntimeDataContainers;
+using Unity.Multiplayer.Samples.Utilities;
+using Unity.Netcode;
+using UnityEngine;
+using VContainer;
+
+namespace Script.GameState
+{
+    public class PostGameState : GameStateBehaviour
+    {
+        [SerializeField] private NetcodeHooks netCodeHooks;
+        
+        [SerializeField] private NetworkPostGameState networkPostGameState;
+        
+        [Inject] private ConnectionManager _connectionManager;
+
+        protected override GameState ActiveState => GameState.GameOver;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            netCodeHooks.OnNetworkSpawnHook += OnNetworkSpawn;
+        }
+        
+        private void OnNetworkSpawn()
+        {
+            if (!NetworkManager.Singleton.IsServer)
+            {
+                return;
+            }
+            
+            var playTime = PlayersStats.Instance.GetPlayTime();
+            networkPostGameState.RpcPlayTimeUpdateClientRpc(playTime);
+        }
+
+        public void OnMainMenuButtonClicked()
+        {
+            _connectionManager.RequestShutdown();
+        }
+    }
+}
