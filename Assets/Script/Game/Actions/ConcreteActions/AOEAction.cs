5749103ca332db03859740d7b8214e431e6da660
Ackrawin B <63220854+BXSS101@users.noreply.github.com>
2023-04-23 20:53:02 +0700

    feat: added AOE SFX

--- Assets/Script/Game/Actions/ConcreteActions/AOEAction.cs
+++ Assets/Script/Game/Actions/ConcreteActions/AOEAction.cs
@@ -92,6 +92,7 @@ namespace Script.Game.Actions.ConcreteActions
                     enemy.ReceiveHP(parent, -Config.Amount);
                 }
             }
+            AudioManager.Instance.SFXSource.PlayOneShot(Config.SoundEffect);
         }
 
         public override bool OnStartClient(ClientCharacter clientCharacter)
